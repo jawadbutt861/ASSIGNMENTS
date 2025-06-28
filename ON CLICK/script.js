af141ee7b4cb2fdb2225bb99309efafad698ca4d
Jawadbutt861 <jawadbutt861@gmail.com>
2025-06-28 13:13:13 +0500

    updated

---
+++ ON CLICK/script.js
@@ -0,0 +1,6 @@
+function clickme(){
+    var click=document.getElementById('text');
+    click.innerHTML="I Love You!";
+    var hide=document.getElementById('click');
+    hide.style.display="none";
+}
---
+++ WHATSAPP/script.js
@@ -0,0 +1,6 @@
+function clickfunction(){
+    var name1=document.getElementById("p-2").innerHTML;
+    document.getElementById("p-3").innerHTML=name1;
+
+}
+
